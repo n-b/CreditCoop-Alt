a855c8f7a394f020214075a1417adab32b860a65
Nicolas Bouilleaud <nicolas.bouilleaud@capitainetrain.com>
2015-07-13 19:27:43 +0200

    Better login/logout observation

--- CreditCoop/AppDelegate.m
+++ CreditCoop/AppDelegate.m
@@ -23,53 +23,38 @@ int main(int argc_, char *argv_[])
     self.creditcoop = [CreditCoop new];
     [self.creditcoop logout]; // Force Delete
     
-    UINavigationController *navigationController = (UINavigationController *)self.window.rootViewController;
-    UserAccountsVC *controller = (UserAccountsVC *)navigationController.topViewController;
-    controller.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Logout"
-                                                                                    style:UIBarButtonItemStylePlain
-                                                                                   target:self
-                                                                                   action:@selector(logout)];
-
-    int64_t delayInSeconds = 1.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        [self.creditcoop addObserver:self
-                          forKeyPath:@"user"
-                             options:NSKeyValueObservingOptionInitial
-                             context:(__bridge void *)([AppDelegate class])];
+    dispatch_async(dispatch_get_main_queue(), ^(void){
+        [self setLoginVCAnimated:NO];
+        [self.creditcoop addObserver:self forKeyPath:@"user" options:0 context:__FILE__];
     });
     return YES;
 }
 
-- (void) logout
+- (IBAction)logout
 {
     [self.creditcoop logout];
 }
 
-- (void) userDidChange
+- (void)setLoginVCAnimated:(BOOL)animated_
 {
     COOUser * user = self.creditcoop.user;
     UINavigationController *navigationController = (UINavigationController *)self.window.rootViewController;
     UserAccountsVC *controller = (UserAccountsVC *)navigationController.topViewController;
     controller.user = user;
     
-    if(user)
-    {
-        [self.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
-    }
-    else
-    {
+    if(user) {
+        [self.window.rootViewController dismissViewControllerAnimated:animated_ completion:nil];
+    } else {
         LoginVC * loginVC = [self.window.rootViewController.storyboard instantiateViewControllerWithIdentifier:@"Login"];
         loginVC.creditcoop = self.creditcoop;
-        [self.window.rootViewController presentViewController:loginVC animated:YES completion:nil];
+        [self.window.rootViewController presentViewController:loginVC animated:animated_ completion:nil];
     }
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath_ ofObject:(id)object_ change:(NSDictionary *)change_ context:(void *)context_
 {
-    if (context_ == (__bridge void *)([AppDelegate class])) {
-        if(object_==self.creditcoop && [keyPath_ isEqualToString:@"user"])
-            [self userDidChange];
+    if (context_==__FILE__) {
+        [self setLoginVCAnimated:YES];
     } else {
         [super observeValueForKeyPath:keyPath_ ofObject:object_ change:change_ context:context_];
     }
